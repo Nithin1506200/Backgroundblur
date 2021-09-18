058c5c7d3e80edeba7e2e6c15693769fb24bc1cb
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2021-09-18 10:57:23 +0530

    resolution change

--- background.js
+++ background.js
@@ -30,7 +30,7 @@ async function keepbackground() {
     architecture: 'MobileNetV1',
     outputStride: 16,
     multiplier: 0.75,
-    quantBytes: 2
+    quantBytes: 1
   });
 const segmentation = await net.segmentPerson(canvas);
 const coloredPartImage = bodyPix.toMask(segmentation);
@@ -44,20 +44,23 @@ const maskBlurAmount = 10;
 
 navigator.getUserMedia=navigator.getUserMedia||navigator.webkitGetUserMedia||navigator.mozGetUserMedia||navigator.msGetUserMedia||navigator.oGetUserMedia;
 if (navigator.getUserMedia) {
-  navigator.getUserMedia({video:true},handleVideo,videoError)
+  navigator.getUserMedia({video:true,width:"100px",height:"100px"},handleVideo,videoError)
 }
 function handleVideo(stream) {
   Video.srcObject=stream;
     let stream_settings = stream.getVideoTracks()[0].getSettings();
-    canvas.width = stream_settings.width;
+ //canvas.width = stream_settings.width;
+   // canvas.height = stream_settings.height;
     canvas2.width=stream_settings.width;
 canvas2.height = stream_settings.height;
-    canvas.height = stream_settings.height;
+   canvas.width = 300;
+    canvas.height = 300;
 
     setInterval(()=> {
- ctx.drawImage(Video,0,0,stream_settings.width,stream_settings.height)
+ //ctx.drawImage(Video,0,0,stream_settings.width/2,stream_settings.height/2)
+ ctx.drawImage(Video,0,0,300,300)
     keepbackground();
-},20)
+},10)
 }
 function videoError() {
   alert("something is wrong")

57faf3ddaabe64eeefa5fdf25ec71612e81ac238
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2021-09-18 11:27:41 +0530

    blur 300 px

--- background.js
+++ background.js
@@ -8,23 +8,7 @@
   const ctx = canvas.getContext('2d');
   let Video=document.querySelector("video");
 
-  
-// function loadImage(src) {
-//   const img = new Image();
-//   img.crossOrigin = '';
 
-//     // Set canvas width, height same as image
-//     // canvas.width = img.width;
-//     // canvas.height = img.height;
-//     ctx.drawImage(img, 0, 0);
-//     keepbackground();
-//   img.src = src;
-// }
-
-
-/**
- * Remove background an image
- */
 async function keepbackground() {
         const net = await bodyPix.load({
     architecture: 'MobileNetV1',
@@ -38,7 +22,7 @@ const opacity = .9;
 const flipHorizontal = false;
 const maskBlurAmount = 10;
 const backgroundBlurAmount = 3;
-const edgeBlurAmount = 3;
+const edgeBlurAmount = 1;
 // bodyPix.drawMask(
   //  canvas2, canvas, coloredPartImage, opacity, maskBlurAmount,
    // flipHorizontal)
@@ -52,7 +36,8 @@ if (navigator.getUserMedia) {
   navigator.getUserMedia({video:true,width:"100px",height:"100px"},handleVideo,videoError)
 }
 function handleVideo(stream) {
-  Video.srcObject=stream;
+ Video.srcObject=stream;
+
     let stream_settings = stream.getVideoTracks()[0].getSettings();
  //canvas.width = stream_settings.width;
    // canvas.height = stream_settings.height;
@@ -60,7 +45,8 @@ function handleVideo(stream) {
 //canvas2.height = stream_settings.height;
    canvas.width = 300;
     canvas.height = 300;
-
+let img1= new Image(100,100);
+img1.src=stream;
     setInterval(()=> {
  //ctx.drawImage(Video,0,0,stream_settings.width/2,stream_settings.height/2)
  ctx.drawImage(Video,0,0,300,300)
